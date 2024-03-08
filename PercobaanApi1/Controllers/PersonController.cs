8a7a487de2b736ac01f32a57485b6a6e731b6b0e
ummunada <ummunada231@gmail.com>
2024-03-08 23:01:52 +0700

    Tugas-PercobaanApi1

---
+++ PercobaanApi1/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+        [HttpGet("api/person/{id}")]
+        public ActionResult<Person> GetPerson(int id)
+        {
+            PersonContext mycontext = new PersonContext();
+            var person = mycontext.ListPerson().FirstOrDefault(p => p.id_person == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+    }
+}
---
+++ PercobaanApi1/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace PercobaanApi1.Models
+{
+    public class Person
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+    }
+}
